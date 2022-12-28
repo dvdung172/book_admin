064f180bdd29b77fa3376b7e3e5043bfbe9a9de8
Z02899 <nguyen.viet.dung@zensho.com>
2022-12-28 21:10:44 +0700

    create

--- lib/presentation/views/products/product_table.dart
+++ lib/presentation/views/products/product_table.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ProductsTable extends StatelessWidget {
@@ -5,70 +7,151 @@ class ProductsTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: const <DataColumn>[
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'ID',
-              style: TextStyle(fontStyle: FontStyle.italic),
-            ),
+    return PaginatedDataTable(columns: const <DataColumn>[
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'ID',
+            style: TextStyle(fontStyle: FontStyle.italic),
           ),
         ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Image',
-              style: TextStyle(fontStyle: FontStyle.italic),
-            ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Image',
+            style: TextStyle(fontStyle: FontStyle.italic),
           ),
         ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Name',
-              style: TextStyle(fontStyle: FontStyle.italic),
-            ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Name',
+            style: TextStyle(fontStyle: FontStyle.italic),
           ),
         ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Price',
-              style: TextStyle(fontStyle: FontStyle.italic),
-            ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Price',
+            style: TextStyle(fontStyle: FontStyle.italic),
           ),
         ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Rating',
-              style: TextStyle(fontStyle: FontStyle.italic),
-            ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Quantity',
+            style: TextStyle(fontStyle: FontStyle.italic),
           ),
         ),
-        DataColumn(
-          label: Expanded(
-            child: Text(
-              'Action',
-              style: TextStyle(fontStyle: FontStyle.italic),
-            ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Sub Category',
+            style: TextStyle(fontStyle: FontStyle.italic),
           ),
         ),
-      ],
-      rows: <DataRow>[
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('1')),
-            DataCell(Image.network(
-                'https://picsum.photos/250?image=9')),
-            DataCell(Text('Harry Porter')),
-            DataCell(Text("\$200")),
-            DataCell(Text('5')),
-            DataCell(Text('Active')),
-          ],
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Created',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
         ),
-      ],
-    );
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Status',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Product Detail',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Action',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+      ),
+    ], source: ProductSource());
   }
+}
+class ProductSource extends DataTableSource{
+
+  final List<Map<String, dynamic>> _data = List.generate(
+      200,
+          (index) => {
+        "id": index,
+        "title": "Item $index",
+        "price": Random().nextInt(10000)
+      });
+  @override
+  DataRow? getRow(int index) {
+    return DataRow(cells: <DataCell>[
+      DataCell(Text('${index}')),
+      DataCell(Image.network('https://picsum.photos/250?image=9')),
+      DataCell(Text('Harry Porter')),
+      DataCell(Text("\$200")),
+      DataCell(Text('50')),
+      DataCell(Text('Trinh thám')),
+      DataCell(Text('20/12/2022')),
+      DataCell(
+        Switch(
+          value: true,
+          activeColor: Colors.blue,
+          onChanged: (bool value) {},
+        ),
+      ),
+      DataCell(
+        OutlinedButton(
+          child: Text('View'),
+          onPressed: () {
+            print('Hello');
+          },
+        ),
+      ),
+      DataCell(Row(
+        children: [
+          IconButton(
+            splashRadius: 17,
+            icon: const Icon(Icons.edit_note_sharp),
+            onPressed: () {},
+          ),
+          IconButton(
+            splashRadius: 17,
+            icon: const Icon(Icons.delete, color: Colors.red,),
+            onPressed: () {},
+          ),
+        ],
+      )),
+    ]);
+  }
+
+  @override
+  // TODO: implement isRowCountApproximate
+  bool get isRowCountApproximate => false;
+
+  @override
+  // TODO: implement rowCount
+  int get rowCount => _data.length;
+
+  @override
+  // TODO: implement selectedRowCount
+  int get selectedRowCount => 0;
+  
 }
--- lib/presentation/views/products/products.dart
+++ lib/presentation/views/products/products.dart
@@ -14,51 +14,53 @@ class _ProductsState extends State<Products> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('All Product', style: CustomTheme.mainTheme.textTheme.headline1),
-                const Padding(
-                  padding: EdgeInsets.only(top: 14),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      width: Responsive.isDesktop(context) ? 500:double.infinity,
-                      height: 36,
-                      child: const TextField(
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.symmetric(vertical: 0),
-                          prefixIcon: Icon(Icons.search),
-                          border: OutlineInputBorder(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('All Product', style: CustomTheme.mainTheme.textTheme.headline1),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 14),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        width: Responsive.isDesktop(context) ? 500:double.infinity,
+                        height: 36,
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(vertical: 0),
+                            prefixIcon: Icon(Icons.search),
+                            border: OutlineInputBorder(
+                            ),
+                            hintText: 'Search Tech Talk',
                           ),
-                          hintText: 'Search Tech Talk',
                         ),
                       ),
-                    ),
-                    ElevatedButton(
-                      child: Text('Add'),
-                      onPressed: () {
-                        print('Hello');
-                      },
-                    ),
-                  ],
-                ),
-              ],
+                      ElevatedButton(
+                        child: Text('Add'),
+                        onPressed: () {
+                          print('Hello');
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 14),
-          ),
-          const ProductsTable(),
-        ],
+            const Padding(
+              padding: EdgeInsets.only(top: 14),
+            ),
+            const ProductsTable(),
+          ],
+        ),
       ),
     );
   }
